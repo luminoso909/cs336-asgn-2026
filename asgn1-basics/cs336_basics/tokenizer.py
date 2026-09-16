6a71f6bea5f88dfdb143e733c2ab9368467b692d
lucius <luminescence909@gmail.com>
2026-09-17 01:01:34 +0800

    初始化，新增asgn1的bpe和tokenizer

---
+++ asgn1-basics/cs336_basics/tokenizer.py
@@ -0,0 +1,24 @@
+from typing import Iterable
+
+class Tokenizer:
+    def __init__(self, vocab, merges, special_tokens=None):
+        pass
+
+
+    
+
+    def encode(self, ):
+        pass
+
+    def encode_iterable(self, iterable:Iterable[str]) -> Iterable[int]:
+        pass
+
+    def decode(self, ):
+        pass
+
+
+def from_files(self, cls, vocab_filepath, merges_filepath, special_tokens = None):
+
+    tokenizer = Tokenizer()
+
+    return tokenizer
